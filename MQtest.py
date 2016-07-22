c1f235ac3a1fb0fab59fc60337bcdd569c02ccba
unknown <>
2016-07-22 13:17:18 +0800

    多线程版本服务器

--- MQtest.py
+++ MQtest.py
@@ -4,14 +4,14 @@
 # topic
 # partition 
 # key
-
+from __future__ import print_function
 import urllib2
 import json
 
 
 def getOffset(host, topic, partition, userKey):
     getUrl = '/'.join([host, 'offsets', topic, str(partition), userKey])
-    print getUrl
+    print(getUrl)
     urlop = urllib2.urlopen(getUrl)
     offsets = json.loads(urlop.read())
     urlop.close()
@@ -21,7 +21,7 @@ def getOffset(host, topic, partition, userKey):
 
 def pullData(host, topic, partition, cur):
     pullUrl = '/'.join([host, 'messages', topic, str(partition), str(cur)])
-    print pullUrl
+    print (pullUrl)
     pull = urllib2.urlopen(pullUrl, timeout=10)
     data = pull.read()
     # print data
@@ -30,7 +30,7 @@ def pullData(host, topic, partition, cur):
 
 def setOffset(host, topic, partition, cur, userKey):
     postUrl = '/'.join([host, 'offsets', topic, str(partition), userKey])
-    print postUrl
+    print (postUrl)
     headers = {"Content-type": "application/json"}
     info = {"offset": cur}
     data = json.dumps(info)
@@ -39,27 +39,29 @@ def setOffset(host, topic, partition, cur, userKey):
     postData.close()
     return 1
 
+
 if __name__ == '__main__':
     offsets = getOffset('http://api.mq.aodianyun.com/v1', 'nodeHls_ttt', 0, 'XZP')
     data = pullData('http://api.mq.aodianyun.com/v1', 'nodeHls_ttt', 0, offsets['offset'])
     # setOffset('http://api.mq.aodianyun.com/v1', 'nodeHls_ttt', 0, 1623204, 'XZP')
-    data_dict=json.loads(data)
+    data_dict = json.loads(data)
     pre_datas = data_dict['list']
     for data in pre_datas:
         # print data
-        body = data.get('body','Error:no body keyword')
+        body = data.get('body', 'Error:no body keyword')
         # body = data['body']
         body_dict = json.loads(body)
         # print body_dict
 
         # {"user": 4285, "time_local": "[15/Jan/2016:15:00:09 +0800]", "unix_time": "1452841209",
         #  "remote_addr": "59.51.90.178", "host": "4285.hlsplay.aodianyun.com",
-        #  "request_url": "GET /1529/1529-stream-1452839610-1452841198584-6084.ts HTTP/1.1", "http_stat": 200,
+        #  "request_url": "GET /1529/1529-stream-1452839610-145284119except socket.timeout:8584-6084.ts HTTP/1.1", "http_stat": 200,
         #  "request_length": 533, "body_bytes_sent": 92684, "http_referer": "-",
         #  "http_user_agent": "Mozilla/5.0 (Linux; U; Android 4.1.1; zh-cn; HUAWEI C8813 Build/HuaweiC8813) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 Chrome/37.0.0.0 MQQBrowser/6.3 Mobile Safari/537.36",
         #  "server_addr": "124.116.157.251", "upstream_addr": "-", "upstream_response_time": "-", "request_time": "0.000"}
         #
         # print body_dict.get('remote_addr','Error:no remote_addr keyword')
         # print body_dict.get('request_length', 'Error:no request.length keyword')
-        size = body_dict.get('body_bytes_sent','Error:no body_bytes_sent keyword')
+        size = body_dict.get('body_bytes_sent', 'Error:no body_bytes_sent keyword')
+        print(size)
         # spark streaming的输入源要求是一个持续不断的输入流，现在有个输入源是restapi格式那种的http请求，
--- socket_server.py
+++ socket_server.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 from __future__ import print_function
-from MQtest import getOffset,setOffset,pullData
+from MQtest import getOffset, setOffset, pullData
 import socket  # socket模块
+import threading
 import json
 import time
+
 HOST = '0.0.0.0'
 PORT = 9999
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 定义socket类型，网络通信，TCP
@@ -11,33 +13,39 @@ s.bind((HOST, PORT))  # 套接字绑定的IP与端口
 s.listen(1)  # 开始TCP监听
 
 
+def response(conn, addr):
+    offsets = getOffset('http://api.mq.aodianyun.com/v1', 'nodeHls_ttt', 0, 'XZP')
+    offset = offsets.get('startOffset', 'Error: no startoffset keykowd in this frame')
+    # 一直发送数据直到游标为空(BUG:不能使用offset来)
+    while offset:
+        try:
+            data = pullData('http://api.mq.aodianyun.com/v1', 'nodeHls_ttt', 0, offset)
+            filter_send_body(data, conn)
+            data = json.loads(data)
+            offset = data.get("nextOffset", "Error: no nextoffset keykowd in this frame")
+            time.sleep(5)
+        except Exception as e:
+            conn.close()
+            print(e)
+            break
+
 def filter_send_body(data, conn):
     data_dict = json.loads(data)
     pre_datas = data_dict['list']
     for data in pre_datas:
         body = data.get('body', 'Error:no body keyword')
         # print(body)
-        conn.send(body+'\n')
-        # body_dict = json.loads(body)
-        # user = body_dict.get('user', 'Error:no user keyword')
-        # flux = body_dict.get('body_bytes_sent', 'Error:no body_bytes_sent keyword')
+        try:
+            conn.send(body + '\n')
+        except Exception as e:
+            conn.close()
+            print(e)
+            break
 
 while 1:
     conn, addr = s.accept()  # 接受TCP连接，并返回新的套接字与IP地址
     print('Connected by', addr)  # 输出客户端的IP地址
-
-    offsets = getOffset('http://api.mq.aodianyun.com/v1', 'nodeHls_ttt', 0, 'XZP')
-    offset = offsets.get('startOffset', 'Error: no startoffset keykowd in this frame')
-    # 持续不断发送数据
-    while offset:
-        data = pullData('http://api.mq.aodianyun.com/v1', 'nodeHls_ttt', 0, offset)
-        filter_send_body(data, conn)
-        data = json.loads(data)
-        offset = data.get("nextOffset","Error: no nextoffset keykowd in this frame")
-        time.sleep(5)
-
-    # 仅发送一次数据
-    # data = pullData('http://api.mq.aodianyun.com/v1', 'nodeHls_ttt', 0, offset)
-    # filter_send_body(data,conn)
+    t = threading.Thread(target=response, args=(conn, addr))
+    t.start()
 
 conn.close()  # 关闭连接
