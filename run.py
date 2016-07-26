0ffaa5fd6faf6244c79c076092bd5d6a2fc9a39e
unknown <>
2016-07-26 17:45:16 +0800

    增加输出方式，输出到本地的mongodb数据库

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ from __future__ import print_function
 
 import sys
 import json
+from pymongo import MongoClient
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -21,38 +22,40 @@ from pyspark.streaming import StreamingContext
 def updateFunc(new_values, last_sum):
     return sum(new_values) + (last_sum or 0)
 
-def data_parse(data):
-    data_dict = json.loads(data)
-    pre_datas = data_dict['list']
-    for data in pre_datas:
-        body = data.get('body', 'Error:no body keyword')
-        body_dict = json.loads(body)
-        user = body_dict.get('user', 'Error:no user keyword')
-        flux = body_dict.get('body_bytes_sent', 'Error:no body_bytes_sent keyword')
-        yield user, flux
+
+def sendPartition(iter):
+    client = MongoClient('localhost', 27017)
+    db = client.hls
+    posts = db.col
+    for record in iter:
+        posts.insert_one({'user': record[0], 'flux': record[1]})
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: flux_analyze.py <hostname> <port>", file=sys.stderr)
         exit(-1)
+
     # Create a local StreamingContext with two working thread and batch interval of 1 second
     # sc = SparkContext('local[2]',appName="HLS_flux_Analyze")
     sc = SparkContext(appName="HLS_Flux_Analyze")
     ssc = StreamingContext(sc, 1)
-    # 并不懂下一句什么意思
     ssc.checkpoint("checkpoint")
 
     # host, port = sys.argv[1:]
     # Create a DStream that will connect to hostname:port
     lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
-    # lines.pprint()
-    # Split each line into word
+
     body_dict = lines.map(lambda s: json.loads(s))
-    #body_dict.pprint()
-    user_flux = body_dict.map(lambda body_dict: (body_dict['user'], body_dict['body_bytes_sent']))
+    user_flux = body_dict.map(
+        lambda body_dict: (body_dict.get('user', "no user keyword"), body_dict['body_bytes_sent']))
+
     running_counts = user_flux.updateStateByKey(updateFunc)
+
+    running_counts.foreachRDD(lambda rdd: rdd.foreachPartition(sendPartition))
     running_counts.pprint()
 
     ssc.start()
     ssc.awaitTermination()
+    ssc.checkpoint("checkpoint")
+
--- test/flux_analyze_test.py
+++ test/flux_analyze_test.py
@@ -4,7 +4,7 @@
  user flux in UTF8 encoded, '\n' delimited text received from the
  network every second.
 
- Usage: flux_analyze.py <hostname> <port>
+ Usage: run.py <hostname> <port>
    <hostname> and <port> describe the TCP server that Spark Streaming
     would connect to receive data.
 """
@@ -21,26 +21,38 @@ from pyspark.streaming import StreamingContext
 def updateFunc(new_values, last_sum):
     return sum(new_values) + (last_sum or 0)
 
+def data_parse(data):
+    data_dict = json.loads(data)
+    pre_datas = data_dict['list']
+    for data in pre_datas:
+        body = data.get('body', 'Error:no body keyword')
+        body_dict = json.loads(body)
+        user = body_dict.get('user', 'Error:no user keyword')
+        flux = body_dict.get('body_bytes_sent', 'Error:no body_bytes_sent keyword')
+        yield user, flux
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: flux_analyze.py <hostname> <port>", file=sys.stderr)
+        print("Usage: run.py <hostname> <port>", file=sys.stderr)
         exit(-1)
-    
     # Create a local StreamingContext with two working thread and batch interval of 1 second
-    # sc = SparkContext('local[2]',appName="HLS_flux_Analyze")
-    sc = SparkContext(appName="HLS_Flux_Analyze")
+    # sc = SparkContext('local[2]',appName="HLS_run")
+    sc = SparkContext(appName="HLS_run")
     ssc = StreamingContext(sc, 1)
+    # 并不懂下一句什么意思
     ssc.checkpoint("checkpoint")
 
     # host, port = sys.argv[1:]
     # Create a DStream that will connect to hostname:port
     lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
-   
+    # lines.pprint()
+    # Split each line into word
     body_dict = lines.map(lambda s: json.loads(s))
+    #body_dict.pprint()
     user_flux = body_dict.map(lambda body_dict: (body_dict['user'], body_dict['body_bytes_sent']))
     running_counts = user_flux.updateStateByKey(updateFunc)
     running_counts.pprint()
 
     ssc.start()
     ssc.awaitTermination()
-
